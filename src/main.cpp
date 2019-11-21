69d4f58f1ce97cd9751ebed4ee89b54c312eb68b
Vegard Seim Karstang <vegaka@stud.ntnu.no>
2019-11-21 09:31:02 +0100

    Added code to benchmark the parallel max flow implementation.

--- src/main.cpp
+++ src/main.cpp
@@ -70,21 +70,48 @@ static void testMF(int argc, char* argv[]) {
     if (argc == 2) {
         CSR_Graph g {argv[1]};
         long src = 0;
-        long sink = 999;
+        long sink = 7999;
 
-        auto pstartTime = std::chrono::high_resolution_clock::now();
-        auto pmaxFlow = PLFFlow(g, src, sink);
-        auto pendTime = std::chrono::high_resolution_clock::now();
+        int iterations = 10;
+        std::vector<long> flows(iterations, 0);
+        std::vector<long> times(iterations);
+        for (size_t i = 0; i < flows.size(); ++i) {
+            auto pstartTime = std::chrono::high_resolution_clock::now();
+            std::cout << "Parallel Max Flow run: " << i << std::endl;
+            flows[i] = PLFFlow(g, src, sink);
+            auto pendTime = std::chrono::high_resolution_clock::now();
+            times[i] = std::chrono::duration_cast<std::chrono::milliseconds>(pendTime - pstartTime).count();
+        }
 
         auto startTime = std::chrono::high_resolution_clock::now();
         auto maxFlow = LFFlow(g, src, sink, true);
         auto endTime = std::chrono::high_resolution_clock::now();
 
+        long errors = 0;
+        for (auto & f: flows) {
+            if (f != maxFlow) {
+                errors++;
+            }
+        }
+
         std::cout << "=======================================" << std::endl;
-        std::cout << "(Parallel) The maximum flow is: " << pmaxFlow << "." << std::endl;
-        std::cout << "(Parallel) Time spent (ms): "
-                  << std::chrono::duration_cast<std::chrono::milliseconds>(pendTime - pstartTime).count()
-                  << std::endl;
+        std::cout << "(Parallel) Flows: [";
+        for (size_t i = 0; i < flows.size() - 1; ++i) {
+            std::cout << flows[i] << ", ";
+        }
+        std::cout << flows[flows.size() - 1] << "]" << std::endl;
+        std::cout << "(Parallel) Errors: " << errors << std::endl;
+
+        std::cout << "(Parallel) Times: [";
+        for (size_t i = 0; i < times.size() - 1; ++i) {
+            std::cout << times[i] << ", ";
+        }
+        std::cout << times[times.size() - 1] << "]" << std::endl;
+
+        //std::cout << "(Parallel) The maximum flow is: " << flows[0] << "." << std::endl;
+        //std::cout << "(Parallel) Time spent (ms): "
+        //          << std::chrono::duration_cast<std::chrono::milliseconds>(pendTime - pstartTime).count()
+        //          << std::endl;
 
         std::cout << "(Single) The maximum flow is: " << maxFlow << "." << std::endl;
         std::cout << "(Single) Time spent (ms): "
